99ad493df3a86c952a8f667a4db869dbb01f0803
Bruno Tenório <tenoriobn@gmail.com>
2023-05-09 14:35:23 -0300

    Criando componente do formulário

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-      <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
+      <Formulario />
     </div>
   );
 }
