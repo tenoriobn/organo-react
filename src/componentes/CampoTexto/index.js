0d9758c409826b5540a1a94142c6dcda457dfa7e
Bruno Tenório <tenoriobn@gmail.com>
2023-05-17 10:41:52 -0300

    Iniciando manipulação do input do usuário com onChange

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,19 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+    const placeholderModificada = `${props.placeholder}...`
+
+    let valor = 'Guilherme Silveira';
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor);
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value ={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
