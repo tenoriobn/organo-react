a28e83f9cb43f46e8f44c8cb3dea885e7d95d750
Bruno Tenório <tenoriobn@gmail.com>
2023-05-18 11:32:49 -0300

    Controlando o estado dos campos e da lista suspensa

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,19 +1,16 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
-    const placeholderModificada = `${props.placeholder}...`
-
-    let valor = 'Guilherme Silveira';
+    const placeholderModificada = `${props.placeholder}...`;
 
     const aoDigitado = (evento) => {
-        valor = evento.target.value;
-        console.log(valor);
+        props.aoAlterado(evento.target.value);
     }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input value ={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
