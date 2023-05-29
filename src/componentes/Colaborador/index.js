77e7371031bb096dab283d3566f09a7cfe77d44f
Bruno Tenório <tenoriobn@gmail.com>
2023-05-29 15:28:29 -0300

    Desenvolvendo card de colaborador com método dumb component

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div className="colaborador">
+            <div className="cabecalho">
+                <img src="https://github.com/tenoriobn.png" alt="Bruno Tenório"></img>
+            </div>
+
+            <div className="rodape">
+                <h4>Bruno Tenório</h4>
+                <h5>Aluno</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
